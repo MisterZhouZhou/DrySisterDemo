b04dc4594c08622f7201e1745731967a4ea9416f
misterzhouzhou <m15038960016@163.com>
2018-03-20 14:11:01 +0800

    模拟第一次点击事件

--- app/src/main/java/com/example/zw/drysisterdemo/MainActivity.java
+++ app/src/main/java/com/example/zw/drysisterdemo/MainActivity.java
@@ -1,60 +1,101 @@
 package com.example.zw.drysisterdemo;
 
 import android.media.Image;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.example.zw.drysisterdemo.API.SisterApi;
+import com.example.zw.drysisterdemo.Model.Sister;
 import com.example.zw.drysisterdemo.Utils.PictureLoader;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button showBtn;
+    private Button refreshBtn;
     private ImageView showImg;
-    private ArrayList<String> urls;
-    private  int curPos = 0;
+    private ArrayList<Sister> data;
+    private  int curPos = 0;  //当前显示的是哪一张
+    private int page = 1;     //当前页数
     private PictureLoader loader;
+    private SisterApi sisterApi;
+    private SisterTask sisterTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sisterApi = new SisterApi();
         loader = new PictureLoader();
         initData();
         initUI();
     }
 
     private void initData(){
-        urls = new ArrayList<String>();
-        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6ipaai7wgj20dw0kugp4.jpg");
-        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6gcxc1t7vj20hs0hsgo1.jpg");
-        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6f5ktcyk0j20u011hacg.jpg");
-        urls.add("http://ww1.sinaimg.cn/large/610dc034jw1f6e1f1qmg3j20u00u0djp.jpg");
-        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6aipo68yvj20qo0qoaee.jpg");
-        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f69c9e22xjj20u011hjuu.jpg");
-        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f689lmaf7qj20u00u00v7.jpg");
-        urls.add("http://ww3.sinaimg.cn/large/c85e4a5cjw1f671i8gt1rj20vy0vydsz.jpg");
-        urls.add("http://ww2.sinaimg.cn/large/610dc034jw1f65f0oqodoj20qo0hntc9.jpg");
-        urls.add("http://ww2.sinaimg.cn/large/c85e4a5cgw1f62hzfvzwwj20hs0qogpo.jpg");
+         data = new ArrayList<>();
+         sisterTask = new SisterTask();
+         sisterTask.execute();
     }
 
     private void initUI(){
         showBtn = (Button) findViewById(R.id.btn_show);
+        refreshBtn = (Button) findViewById(R.id.btn_refresh);
         showImg = (ImageView) findViewById(R.id.img_show);
         showBtn.setOnClickListener(this);
+        refreshBtn.setOnClickListener(this);
+    }
+
+    private class SisterTask extends AsyncTask<Void,Void,ArrayList<Sister>>{
+        public SisterTask( ){}
+
+        @Override
+        protected ArrayList<Sister> doInBackground(Void... voids) {
+            return sisterApi.fetchSister(10,page);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Sister> sisters) {
+            super.onPostExecute(sisters);
+            data.clear();
+            data.addAll(sisters);
+            // 模拟第一次点击
+            showBtn.performClick();
+            page++;
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            sisterTask = null;
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_show:
-                if(curPos > 9){
-                    curPos = 0;
+                if(data != null && !data.isEmpty()){
+                    if(curPos > 9){
+                        curPos = 0;
+                    }
+                    loader.load(showImg, data.get(curPos).getUrl());
+                    curPos++;
                 }
-                loader.load(showImg, urls.get(curPos));
+                break;
+            case R.id.btn_refresh:
+                sisterTask = new SisterTask();
+                sisterTask.execute();
+                curPos = 0;
                 break;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sisterTask.cancel(true);
     }
 }
